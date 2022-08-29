ffb2f36a29f81b18a3c4de397ac18888cbae6d10
xujianxing <xjx_me@yeah.net>
2022-08-29 14:34:09 +0700

    init

---
+++ index.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const orderDetail = require('./orderDetail.json');
+const cartDetail = require('./cartDetail.json');
+const app = express();
+
+app.get('/order-cart-api/order/orderDetail', (req, res) => {
+  res.json(orderDetail);
+})
+
+app.get('/order-cart-api//cart/cartDetail', (req, res) => {
+  res.json(cartDetail);
+})
+
